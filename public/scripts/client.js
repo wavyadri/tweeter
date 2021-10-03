e7e645a0f156d011a9747be25f3c91a39cf452d1
Adriana <adriana.calvomatos@gmail.com>
2021-10-03 04:26:45 +0000

    clean up code in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,41 +1,31 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// questions:
-// should the function definitions be inside doc.ready()?
-// should I refactor createTweetElement so it creates a div, appends children etc etc?
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac',
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd',
+    },
+    content: {
+      text: 'Je pense , donc je suis',
+    },
+    created_at: 1461113959088,
+  },
+];
 
 // returns a tweet <article> element containing the entire HTML structure of the tweet
 $(document).ready(function () {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      user: {
-        name: 'Newton',
-        avatars: 'https://i.imgur.com/73hZDYK.png',
-        handle: '@SirIsaac',
-      },
-      content: {
-        text: 'If I have seen further it is by standing on the shoulders of giants',
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: 'Descartes',
-        avatars: 'https://i.imgur.com/nlhLi3I.png',
-        handle: '@rd',
-      },
-      content: {
-        text: 'Je pense , donc je suis',
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const renderTweets = function (tweets) {
     // loops through tweets
     for (const tweet of tweets) {
@@ -83,21 +73,3 @@ $(document).ready(function () {
     e.preventDefault();
   });
 });
-
-////////////////////
-// const tweetData = {
-//   user: {
-//     name: 'Newton',
-//     avatars: 'https://i.imgur.com/73hZDYK.png',
-//     handle: '@SirIsaac',
-//   },
-//   content: {
-//     text: 'If I have seen further it is by standing on the shoulders of giants',
-//   },
-//   created_at: 1461116232227,
-// };
-
-// const $tweet = createTweetElement(tweetData);
-// // Test / driver code (temporary)
-// console.log('$tweet:', $tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
