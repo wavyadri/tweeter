7beba5310b69f55052a290b782783803d36355c5
Adriana <adriana.calvomatos@gmail.com>
2021-10-03 04:02:25 +0000

    implement renderTweets to loop through all tweet data and utilize createTweetElement to append to DOM node

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// questions:
+// should the function definitions be inside doc.ready()?
+// should I refactor createTweetElement so it creates a div, appends children etc etc?
+
 // returns a tweet <article> element containing the entire HTML structure of the tweet
 
 $(document).ready(function () {
@@ -17,6 +21,42 @@ $(document).ready(function () {
       text: 'If I have seen further it is by standing on the shoulders of giants',
     },
     created_at: 1461116232227,
+  };
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      user: {
+        name: 'Newton',
+        avatars: 'https://i.imgur.com/73hZDYK.png',
+        handle: '@SirIsaac',
+      },
+      content: {
+        text: 'If I have seen further it is by standing on the shoulders of giants',
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: 'Descartes',
+        avatars: 'https://i.imgur.com/nlhLi3I.png',
+        handle: '@rd',
+      },
+      content: {
+        text: 'Je pense , donc je suis',
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $newTweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($newTweet);
+    }
   };
 
   const createTweetElement = (tweet) => {
@@ -49,9 +89,11 @@ $(document).ready(function () {
   `;
   };
 
-  const $tweet = createTweetElement(tweetData);
+  // const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  console.log('$tweet:', $tweet); // to see what it looks like
-  $('#tweetr-feed').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+
+  // // Test / driver code (temporary)
+  // console.log('$tweet:', $tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
