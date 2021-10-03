d2c70b6d9e87191c62fb904e84c9525f326c9d85
Adriana <adriana.calvomatos@gmail.com>
2021-10-03 07:00:52 +0000

    clear tweet container before rendering tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,33 @@
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: 'Newton',
+//       avatars: 'https://i.imgur.com/73hZDYK.png',
+//       handle: '@SirIsaac',
+//     },
+//     content: {
+//       text: 'If I have seen further it is by standing on the shoulders of giants',
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: 'Descartes',
+//       avatars: 'https://i.imgur.com/nlhLi3I.png',
+//       handle: '@rd',
+//     },
+//     content: {
+//       text: 'Je pense , donc je suis',
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
 // returns a tweet <article> element containing HTML structure of the tweet
 $(document).ready(function () {
   const renderTweets = function (tweets) {
+    $('#tweets-container').empty();
     for (const tweet of tweets) {
       const $newTweet = createTweetElement(tweet);
       $('#tweets-container').append($newTweet);
@@ -63,7 +64,7 @@ $(document).ready(function () {
   `;
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
   $('#tweet-form').submit((e) => {
     e.preventDefault();
