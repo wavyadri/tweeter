9b14b3d2183316b82600887d800f63411f530c78
Adriana <adriana.calvomatos@gmail.com>
2021-10-03 03:31:47 +0000

    render tweets with temp data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,50 +5,53 @@
  */
 
 // returns a tweet <article> element containing the entire HTML structure of the tweet
-const createTweetElement = (tweet) => {
-  const $tweet = $(`<article class="single-tweet">
-  <header>
-    <div class="user">
-      <img
-        src="https://i.imgur.com/nlhLi3I.png"
-        alt="Descartes' avatar"
-      />
-      <h4>Descartes</h4>
-    </div>
-    <div class="user-tag">
-      <h4>@rd</h4>
-    </div>
-  </header>
-  <p class="user-tweet">Je pense , donc je suis</p>
-  <footer>
-    <div class="date">
-      <p>12 days ago</p>
-    </div>
-    <div class="icons">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </div>
-  </footer>
-</article>`);
-};
-$(document).ready(function () {});
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  user: {
-    name: 'Newton',
-    avatars: 'https://i.imgur.com/73hZDYK.png',
-    handle: '@SirIsaac',
-  },
-  content: {
-    text: 'If I have seen further it is by standing on the shoulders of giants',
-  },
-  created_at: 1461116232227,
-};
+$(document).ready(function () {
+  const tweetData = {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac',
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  };
 
-const $tweet = createTweetElement(tweetData);
+  const createTweetElement = (tweet) => {
+    return `
+    <article class="single-tweet">
+      <header>
+        <div class="user">
+          <img
+            src=${tweet.user.avatars}
+            alt="${tweet.user.name} avatar"
+          />
+          <h4>${tweet.user.name} </h4>
+        </div>
+        <div class="user-tag">
+          <h4>${tweet.user.handle} </h4>
+        </div>
+      </header>
+      <p class="user-tweet">${tweet.content.text} </p>
+      <footer>
+        <div class="date">
+          <p>${tweet.created_at}</p>
+        </div>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `;
+  };
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log('$tweet:', $tweet); // to see what it looks like
+  $('#tweetr-feed').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
