9d81ac6e0466894a92957ebf329c2a2a6545d09f
Adriana <adriana.calvomatos@gmail.com>
2021-10-03 04:26:00 +0000

    remove static tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,20 +9,7 @@
 // should I refactor createTweetElement so it creates a div, appends children etc etc?
 
 // returns a tweet <article> element containing the entire HTML structure of the tweet
-
 $(document).ready(function () {
-  const tweetData = {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  };
-
   // Fake data taken from initial-tweets.json
   const data = [
     {
@@ -89,11 +76,28 @@ $(document).ready(function () {
   `;
   };
 
-  // const $tweet = createTweetElement(tweetData);
-
   renderTweets(data);
 
-  // // Test / driver code (temporary)
-  // console.log('$tweet:', $tweet); // to see what it looks like
-  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  ///////////////////////////////////////////////////////////////////
+  $('#tweet-form').submit((e) => {
+    e.preventDefault();
+  });
 });
+
+////////////////////
+// const tweetData = {
+//   user: {
+//     name: 'Newton',
+//     avatars: 'https://i.imgur.com/73hZDYK.png',
+//     handle: '@SirIsaac',
+//   },
+//   content: {
+//     text: 'If I have seen further it is by standing on the shoulders of giants',
+//   },
+//   created_at: 1461116232227,
+// };
+
+// const $tweet = createTweetElement(tweetData);
+// // Test / driver code (temporary)
+// console.log('$tweet:', $tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
